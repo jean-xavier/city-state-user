f26c5eff302527c123a3f9901f992bd673414b27
Jean Xavier <jxs.jean@gmail.com>
2021-02-06 16:34:55 -0300

    Initial commit.

---
+++ src/main/java/com/challenge/citystateuser/CityStateUserApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.citystateuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityStateUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityStateUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/citystateuser/CityStateUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.citystateuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityStateUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
