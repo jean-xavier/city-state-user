60bc50449ae9da9cedebc54771f49a36998a06a5
Jean Xavier <jxs.jean@gmail.com>
2021-02-07 11:32:56 -0300

    Implements save and findByName methods in CityServiceImpl.

--- src/main/java/com/challenge/citystateuser/domain/services/impl/CityServiceImpl.java
+++ src/main/java/com/challenge/citystateuser/domain/services/impl/CityServiceImpl.java
@@ -1,6 +1,8 @@
 package com.challenge.citystateuser.domain.services.impl;
 
+import com.challenge.citystateuser.domain.exceptions.BusinessException;
 import com.challenge.citystateuser.domain.models.entities.City;
+import com.challenge.citystateuser.domain.models.repositories.CityRepository;
 import com.challenge.citystateuser.domain.services.CityService;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,26 @@ import java.util.Optional;
 
 @Service
 public class CityServiceImpl implements CityService {
+
+    private final CityRepository cityRepository;
+
+    public CityServiceImpl(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
     @Override
     public City save(City city) {
-        return null;
+        final Boolean exists = cityRepository.existsByName(city.getName());
+
+        if (exists) {
+            throw new BusinessException("City already exists");
+        }
+
+        return cityRepository.save(city);
     }
 
     @Override
     public Optional<City> findByName(String name) {
-        return Optional.empty();
+        return cityRepository.findByName(name);
     }
 }
