35e65d6cb33ad642e4fedbdaa0cdb060c044ac5b
Jean Xavier <jxs.jean@gmail.com>
2021-02-06 22:15:20 -0300

    Implements integration tests for feat 'search by name' endpoint.

--- src/test/java/com/challenge/citystateuser/api/controllers/CityControllerTest.java
+++ src/test/java/com/challenge/citystateuser/api/controllers/CityControllerTest.java
@@ -3,7 +3,6 @@ package com.challenge.citystateuser.api.controllers;
 import com.challenge.citystateuser.api.dto.CityDTO;
 import com.challenge.citystateuser.domain.models.entities.City;
 import com.challenge.citystateuser.domain.services.CityService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,6 +19,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -48,7 +49,7 @@ public class CityControllerTest {
 
         String json = new ObjectMapper().writeValueAsString(cityDTO);
 
-        MockHttpServletRequestBuilder request = makeMockHttpServletRequestBuilder(json);
+        MockHttpServletRequestBuilder request = makePostMockHttpServletRequestBuilder(json);
 
         mvc.perform(request)
                 .andExpect(status().isCreated())
@@ -58,21 +59,58 @@ public class CityControllerTest {
 
     @Test
     @DisplayName("Deve retornar erros ao cria uma cidade com dados vazios")
-    public void createInvalidCity() throws Exception {
+    public void createInvalidCityTest() throws Exception {
         String json = new ObjectMapper().writeValueAsString(new CityDTO());
 
-        MockHttpServletRequestBuilder request = makeMockHttpServletRequestBuilder(json);
+        MockHttpServletRequestBuilder request = makePostMockHttpServletRequestBuilder(json);
 
         mvc.perform(request)
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("errors", hasSize(2)));
     }
 
-    private MockHttpServletRequestBuilder makeMockHttpServletRequestBuilder(String json) {
+    @Test
+    @DisplayName("Deve retornar uma cidade pelo nome")
+    public void searchCityByNameTest() throws Exception {
+        City city = City.builder().id(1L).name("Salvador").idState(1L).build();
+
+        BDDMockito.when(cityService.findByName(city.getName())).thenReturn(Optional.of(city));
+
+        final String query = String.format("?name=%s", city.getName());
+
+        MockHttpServletRequestBuilder request = makeGetMockHttpServletRequestBuilder(query);
+
+        mvc.perform(request)
+                .andExpect(status().isFound())
+                .andExpect(jsonPath("id").value(1L))
+                .andExpect(jsonPath("name").value(city.getName()));
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro quando procurar uma cidade inexistente")
+    public void searchInvalidCityByNameTest() throws Exception {
+        City city = City.builder().id(1L).name("Salvador").idState(1L).build();
+
+        BDDMockito.when(cityService.findByName(city.getName())).thenReturn(Optional.empty());
+
+        final String query = String.format("?name=%s", city.getName());
+
+        MockHttpServletRequestBuilder request = makeGetMockHttpServletRequestBuilder(query);
+
+        mvc.perform(request).andExpect(status().isNotFound());
+    }
+
+    private MockHttpServletRequestBuilder makePostMockHttpServletRequestBuilder(String json) {
         return MockMvcRequestBuilders
                 .post(CITY_API)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .content(json);
+    }
+
+    private MockHttpServletRequestBuilder makeGetMockHttpServletRequestBuilder(String query) {
+        return MockMvcRequestBuilders
+                .get(CITY_API + query)
+                .accept(MediaType.APPLICATION_JSON);
     }
 }
