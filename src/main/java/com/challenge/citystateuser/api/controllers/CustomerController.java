f462c5effaf753c31c1284ffa5d663fcfb37f907
Jean Xavier <jxs.jean@gmail.com>
2021-02-09 00:27:39 -0300

    Add customer controller with create endpoint.

---
+++ src/main/java/com/challenge/citystateuser/api/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package com.challenge.citystateuser.api.controllers;
+
+import com.challenge.citystateuser.api.dto.CustomerDTO;
+import com.challenge.citystateuser.domain.models.entities.Customer;
+import com.challenge.citystateuser.domain.services.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+    private final ModelMapper modelMapper;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+        this.modelMapper = new ModelMapper();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerDTO create(@RequestBody @Valid CustomerDTO customerDTO) {
+        Customer customer = modelMapper.map(customerDTO, Customer.class);
+
+        customer = customerService.save(customer);
+
+        return modelMapper.map(customer, CustomerDTO.class);
+    }
+}
