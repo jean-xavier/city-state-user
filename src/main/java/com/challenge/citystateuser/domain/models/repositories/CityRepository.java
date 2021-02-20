65099de5375ed0d3b3cd21d20ee053da3bf83a64
Jean Xavier <jxs.jean@gmail.com>
2021-02-19 23:30:18 -0300

    Remove unnecessary methods.

--- src/main/java/com/challenge/citystateuser/domain/models/repositories/CityRepository.java
+++ src/main/java/com/challenge/citystateuser/domain/models/repositories/CityRepository.java
@@ -4,11 +4,6 @@ import com.challenge.citystateuser.domain.models.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
-    Boolean existsByName(String name);
-
-    Optional<City> findByName(String name);
 }
