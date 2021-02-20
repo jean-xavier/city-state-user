61cd4a7b9e9c2488badf220bdfc8df87616105ae
Jean Xavier <jxs.jean@gmail.com>
2021-02-19 23:33:17 -0300

    Refactoring save and find methods. Inject StateRepository.

--- src/main/java/com/challenge/citystateuser/domain/services/impl/CityServiceImpl.java
+++ src/main/java/com/challenge/citystateuser/domain/services/impl/CityServiceImpl.java
@@ -2,8 +2,14 @@ package com.challenge.citystateuser.domain.services.impl;
 
 import com.challenge.citystateuser.domain.exceptions.BusinessException;
 import com.challenge.citystateuser.domain.models.entities.City;
+import com.challenge.citystateuser.domain.models.entities.State;
 import com.challenge.citystateuser.domain.models.repositories.CityRepository;
+import com.challenge.citystateuser.domain.models.repositories.StateRespository;
 import com.challenge.citystateuser.domain.services.CityService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,24 +18,31 @@ import java.util.Optional;
 public class CityServiceImpl implements CityService {
 
     private final CityRepository cityRepository;
+    private final StateRespository stateRespository;
 
-    public CityServiceImpl(CityRepository cityRepository) {
+    public CityServiceImpl(CityRepository cityRepository, StateRespository stateRespository) {
         this.cityRepository = cityRepository;
+        this.stateRespository = stateRespository;
     }
 
     @Override
     public City save(City city) {
-        final Boolean exists = cityRepository.existsByName(city.getName());
+        Optional<State> state = stateRespository.findById(city.getState().getId());
 
-        if (exists) {
-            throw new BusinessException("City already exists");
-        }
-
-        return cityRepository.save(city);
+        return state.map(stateFound -> {
+            city.setState(stateFound);
+            return cityRepository.save(city);
+        }).orElseThrow(() -> new BusinessException("State not found"));
     }
 
     @Override
-    public Optional<City> findByName(String name) {
-        return cityRepository.findByName(name);
+    public Page<City> findByName(City city, Pageable pageable) {
+        Example<City> example = Example.of(city,
+                ExampleMatcher.matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.EXACT));
+
+        return cityRepository.findAll(example, pageable);
     }
 }
