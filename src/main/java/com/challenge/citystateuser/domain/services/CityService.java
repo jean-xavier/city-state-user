4226f6ea82ccf753a368f6cc52ddef915366e6f9
Jean Xavier <jxs.jean@gmail.com>
2021-02-19 23:31:19 -0300

    Add pageable as parameter.

--- src/main/java/com/challenge/citystateuser/domain/services/CityService.java
+++ src/main/java/com/challenge/citystateuser/domain/services/CityService.java
@@ -1,13 +1,13 @@
 package com.challenge.citystateuser.domain.services;
 
 import com.challenge.citystateuser.domain.models.entities.City;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public interface CityService {
     City save(City city);
 
-    Optional<City> findByName(String name);
+    Page<City> findByName(City city, Pageable pageable);
 }
